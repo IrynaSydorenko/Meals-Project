6557be6a03b1a71463e958347e2728b602b850e7
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2025-01-15 23:18:03 +0000

    feat[client]: created MealsGrid component

---
+++ app/components/meals/MealsGrid.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MealItem from './MealItem';
+import styles from './MealsGrid.module.css';
+
+function MealsGrid({ meals }) {
+  return (
+    <ul className={styles.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default MealsGrid;
