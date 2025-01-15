194b21d2b8b9254975d6b1372597fbf6c9381def
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2025-01-15 23:20:13 +0000

    feat[client]: Created MealsPage

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Link from 'next/link';
+import { getMeals } from '../../lib/meals';
+import MealsGrid from '../components/meals/MealsGrid';
+import styles from './MealsPage.module.css';
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delesious meals created{' '}
+          <span className={styles.highlight}>By you</span>
+        </h1>
+        <p>Choose your favourite recepie and cook it yourself</p>
+        <p className={styles.cta}>
+          <Link href='/meals/share'>Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
 
 export default MealsPage;
