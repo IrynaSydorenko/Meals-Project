d9ca4cb1897ad7903c70d45772b0e7b51ac2e964
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2025-01-15 23:20:56 +0000

    feat[client]: created MealDetails page component

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
+import styles from './MealDetails.module.css';
+import { notFound } from 'next/navigation';
 
-function page() {
-  return <div>Slug page</div>;
+async function MealDetailsPage({ params }) {
+  const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
-export default page;
+export default MealDetailsPage;
