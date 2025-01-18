63cdbb0663f95841410eaf0c2451d7f83a01d345
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2025-01-18 10:28:38 +0000

    feat[client]: Added error related to share page

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+function Error() {
+  return (
+    <main className='error'>
+      <h1>An error occurred</h1>
+      <p>Faild to create meal.</p>
+    </main>
+  );
+}
+
+export default Error;
